47a718b8f2f61c62cf05aae60bb3a2fa1cdd74b1
Yu Jun <yujun_daxia@163.com>
2015-10-24 21:03:20 +0800

    delete all

--- ass2mvc/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ass2mvc.MvcApplication" Language="C#" %>
--- ass2mvc/Models/cake.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Benjamin\Desktop\26346702_ass2mvc\ass2mvc\Models\cake.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
